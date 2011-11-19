d72a36642504415010f3c191787c866108c6f3ec
MrMuggles <bob.hall88@gmail.com>
2011-11-18 21:57:58 -0600

    Commit accidental commit in AndroidManifest

--- gen/com/songbook/R.java
+++ gen/com/songbook/R.java
@@ -14,14 +14,14 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int getLyricsButton=0x7f060001;
-        public static final int instant_search=0x7f060007;
-        public static final int rowSongInfo=0x7f060004;
-        public static final int rowSongLyrics=0x7f060003;
-        public static final int rowSongTitle=0x7f060002;
-        public static final int searchFieldEditText=0x7f060000;
-        public static final int songLyricsAndInfo=0x7f060006;
-        public static final int songTitle=0x7f060005;
+        public static final int getLyricsButton=0x7f070001;
+        public static final int preferences=0x7f070007;
+        public static final int rowSongInfo=0x7f070004;
+        public static final int rowSongLyrics=0x7f070003;
+        public static final int rowSongTitle=0x7f070002;
+        public static final int searchFieldEditText=0x7f070000;
+        public static final int songLyricsAndInfo=0x7f070006;
+        public static final int songTitle=0x7f070005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -29,11 +29,14 @@ public final class R {
         public static final int song=0x7f030002;
     }
     public static final class menu {
-        public static final int options_menu=0x7f050000;
+        public static final int options_menu=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int no_songs=0x7f040002;
-        public static final int search_text=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int no_songs=0x7f050002;
+        public static final int search_text=0x7f050001;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
