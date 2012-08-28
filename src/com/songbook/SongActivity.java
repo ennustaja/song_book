666db83ac095424315eae87eb52003068557a4c7
kv0th3 <dean.kariniemi@gmail.com>
2012-08-28 13:25:31 +0300

    Added song options menu with Previous and Next songs

--- src/com/songbook/SongActivity.java
+++ src/com/songbook/SongActivity.java
@@ -2,11 +2,16 @@ package com.songbook;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.text.Html;
 import android.text.Spanned;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class SongActivity extends Activity{
@@ -15,6 +20,11 @@ public class SongActivity extends Activity{
 	@SuppressWarnings("unused")
 	private static final String TAG = "SongBook";
 
+	private SongDbAdapter mDbHelper;
+	private Cursor mSongsCursor;
+	private boolean SL, SHZ, V;
+	private String number;
+	
 	/* View items */
 	private TextView songTitle;
 	private TextView songLyricsAndInfo;
@@ -50,7 +60,17 @@ public class SongActivity extends Activity{
 	
 	private void displaySong(){
 		String type = getIntent().getStringExtra("type");
-		String number = getIntent().getStringExtra("number");
+		if(type.equals(SongDbAdapter.TYPE_SIIONIN_LAULU)){
+			SL = true;
+			SHZ = V = false;
+		} else if(type.equals(SongDbAdapter.TYPE_VIRSI)){
+			V = true;
+			SHZ = SL = false;
+		} else {
+			SHZ = true;
+			SL = V = false;
+		}
+		number = getIntent().getStringExtra("number");
 		String lyrics = getIntent().getStringExtra("lyrics");
 		String info = getIntent().getStringExtra("info");
 
@@ -88,5 +108,69 @@ public class SongActivity extends Activity{
 
 		return Html.fromHtml(parsedLyrics +
 				"<br />&nbsp;&nbsp;<small><font color='0x666666'>" + parsedInfo + "</small></font>");
+	}
+	
+	/**
+	 * Creates options menu from options_menu template.
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.song_options_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Integer song_number = Integer.parseInt(number);
+		Intent songIntent = new Intent();
+		
+		songIntent.setClassName("com.songbook", "com.songbook.SongActivity");
+
+		mDbHelper = new SongDbAdapter(this);
+		mDbHelper.open();
+		
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.previous_song:
+			song_number -= 1;
+			if (song_number > 0) {
+				mSongsCursor = mDbHelper.fetchSongsByNumber(song_number, SL, V, SHZ);
+				if(mSongsCursor != null && mSongsCursor.getCount() > 0){
+					mSongsCursor.moveToFirst();
+					songIntent.putExtra("type", mSongsCursor.getString(mSongsCursor.getColumnIndex(SongDbAdapter.KEY_TYPE)));
+					songIntent.putExtra("number", mSongsCursor.getString(mSongsCursor.getColumnIndex(SongDbAdapter.KEY_NUMBER)));
+					songIntent.putExtra("lyrics", mSongsCursor.getString(mSongsCursor.getColumnIndex(SongDbAdapter.KEY_LYRICS)));
+					songIntent.putExtra("info", mSongsCursor.getString(mSongsCursor.getColumnIndex(SongDbAdapter.KEY_INFO)));
+	
+					mDbHelper.close();
+					songIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					startActivity(songIntent);
+				} else {
+					mDbHelper.close();
+				}
+			}
+			return true;
+		case R.id.next_song:
+			song_number += 1;
+			
+			mSongsCursor = mDbHelper.fetchSongsByNumber(song_number, SL, V, SHZ);
+			if(mSongsCursor != null && mSongsCursor.getCount() > 0){
+				mSongsCursor.moveToFirst();
+				songIntent.putExtra("type", mSongsCursor.getString(mSongsCursor.getColumnIndex(SongDbAdapter.KEY_TYPE)));
+				songIntent.putExtra("number", mSongsCursor.getString(mSongsCursor.getColumnIndex(SongDbAdapter.KEY_NUMBER)));
+				songIntent.putExtra("lyrics", mSongsCursor.getString(mSongsCursor.getColumnIndex(SongDbAdapter.KEY_LYRICS)));
+				songIntent.putExtra("info", mSongsCursor.getString(mSongsCursor.getColumnIndex(SongDbAdapter.KEY_INFO)));
+	
+				mDbHelper.close();
+				songIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(songIntent);
+			} else {
+				mDbHelper.close();
+			}
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
