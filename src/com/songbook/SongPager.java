72473e00192fe7dfc9a2fc2bbd75a51ce6b6a121
MrMuggles <bob.hall88@gmail.com>
2014-01-14 23:25:34 -0600

    I think immersive mode sometimes not triggering is an android bug, but changed to not not have content cut off when that happens. Also removed some padding.

--- src/com/songbook/SongPager.java
+++ src/com/songbook/SongPager.java
@@ -44,11 +44,10 @@ public class SongPager extends FragmentActivity {
 		super.onWindowFocusChanged(hasFocus);
 		if (hasFocus && Build.VERSION.SDK_INT >= 19) {
 			mPager.setSystemUiVisibility(
-							View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-							| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-							| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-							| View.SYSTEM_UI_FLAG_FULLSCREEN
-							| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+					View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		}
 	}
 
 	public static class MyAdapter extends FragmentStatePagerAdapter {
