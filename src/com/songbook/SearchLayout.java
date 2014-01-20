f45917364a7269848df591d761ddd73e2257307d
MrMuggles <bob.hall88@gmail.com>
2014-01-19 21:57:31 -0600

    Translucent navigation on main list.

---
+++ src/com/songbook/SearchLayout.java
@@ -0,0 +1,33 @@
+package com.songbook;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by bob on 1/19/14.
+ */
+public class SearchLayout extends LinearLayout {
+	public SearchLayout(Context context) {
+		super(context);
+	}
+
+	public SearchLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public SearchLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	protected boolean fitSystemWindows(Rect insets) {
+		LinearLayout.LayoutParams params = ((LinearLayout.LayoutParams)this.getLayoutParams());
+		int bottom = params.bottomMargin;
+		int left = params.leftMargin;
+		int right = params.rightMargin;
+		params.setMargins(left, insets.top, right, bottom);
+		return super.fitSystemWindows(insets);
+	}
+}
